5a15a2b43c16b1e2b712490f2e770b315943925e
jaumemy <jaume.montane@outlook.com>
2020-12-14 10:57:26 -0500

    Part 10 - Create Update & Delete (CRUD)

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     # path('home/', views.home, name="home"),
     path('products/', views.products, name="products"),
     path('customers/<str:pk>/', views.customers, name="customers"),
+    path('create_order/', views.create_order, name="create_order"),
+    path('update_order/<str:pk>', views.update_order, name="update_order"),
+    path('delete_order/<str:pk>', views.delete_order, name="delete_order"),
 ]
