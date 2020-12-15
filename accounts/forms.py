d03966ef6d56ea850d56ebafad84953f10a3c588
jaumemy <jaume.montane@outlook.com>
2020-12-15 17:25:13 -0500

    Part 14 - User Registration and Authentication

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,16 @@
 from django.forms import ModelForm
 from .models import Order
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
 
 
-class OrderForm(ModelForm):
+class order_form(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
+
+class create_user_form(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
--- accounts/urls.py
+++ accounts/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('create_order/<str:pk>/', views.create_order, name="create_order"),
     path('update_order/<str:pk>/', views.update_order, name="update_order"),
     path('delete_order/<str:pk>/', views.delete_order, name="delete_order"),
+    path('register/', views.register_page, name="register"),
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout"),
 ]
