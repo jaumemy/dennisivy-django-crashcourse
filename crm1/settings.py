927ab308e7d859e1dc7519b22a124473f8adfb2e
jaumemy <jaume.montane@outlook.com>
2020-12-29 07:45:23 -0500

    .

--- crm1/settings.py
+++ crm1/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 }
 
 import dj_database_url
-cb_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
